288b2539153f541cf04f424b6736f6df09a1dac2
Rikuba <rikuba@live.jp>
2020-10-19 03:58:05 +0900

    faviconとサムネイル画像のキャッシュストレージを分ける
    ライフサイクルが違うため

--- packages/hosting/api.ts
+++ packages/hosting/api.ts
@@ -1,11 +1,11 @@
 import { Channel, SiteId } from '@aoe2-live/common';
 
 const API_BASE = 'https://aoe2.live/api';
-const CACHE_BASE = 'https://storage.googleapis.com/aoe2-live-cache';
+const FAVICON_BASE = 'https://storage.googleapis.com/aoe2-live.appspot.com';
 
 const json = (url: string) => fetch(url).then((res) => res.json());
 
 export const channels = () =>
   json(`${API_BASE}/channels`) as Promise<Channel[]>;
 
-export const favicon = (site: SiteId) => `${CACHE_BASE}/favicon.${site}.png`;
+export const favicon = (site: SiteId) => `${FAVICON_BASE}/favicon.${site}.png`;
